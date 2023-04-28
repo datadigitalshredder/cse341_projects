8702f83c18255b6d29a131a3b710783c0f5c6121
datadigitalshredder <hov21003@byui.edu>
2023-04-28 16:30:26 +0200

    Lesson 2 start

--- DBconnection/connection.js
+++ DBconnection/connection.js
@@ -1,10 +1,33 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://cse341projects:hove101cse341@cluster0.xptqsz6.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://cse341projects:hove101cse341@cluster0.xptqsz6.mongodb.net/?retryWrites=true&w=majority";
 
-const connectDB = async () => {
-    await mongoose.connect(uri);
-    console.log('DB Connected!');
-}
+// const connectDB = async () => {
+//     await mongoose.connect(uri);
+//     console.log('DB Connected!');
+// }
 
-module.exports = connectDB;
+// module.exports = connectDB;
+
+// CONNECT TO MONGODB - TEAM SOLUTION
+
+const {MongoClient} = require('mongodb');
+
+const conncetDB = async () => {
+    try {
+        const client = await MongoClient.conncetDB(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        const db = client.db(process.env.DB_NAME);
+        const contactsCollection = db.collection(process.env.DB_COLLECTION_NAME);
+        console.log('MongoDB Connected!');
+        return contactsCollection;
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
+
+module.exports = { conncetDB };
+
